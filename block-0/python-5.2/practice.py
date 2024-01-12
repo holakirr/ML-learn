075ed01de930dd402fd9433d7f14c91603f49229
Kirill Petunin <kpetunin@proton.me>
2024-01-12 10:22:21 -0600

    feat(block_0_python_5_2): :sparkles: adds final task related to register function

--- block-0/python-5.2/practice.py
+++ block-0/python-5.2/practice.py
@@ -180,3 +180,66 @@ ic(check_date(13, 13, 2021))
 # False
 ic(check_date(13.5, 12, 2021))
 # False
+
+print("#" * 40)
+# Нам остался финальный штрих — добавить проверку даты на корректность в функцию для регистрации!
+
+
+def register(surname, name, date, middle_name=None, registry=None):
+    # Вспомогательная функция для предобработки даты
+    def preprocessing_date(date):
+        # Разделяем строку по символу точки
+        day, month, year = date.split(".")
+        # Преобразуем все данные к типу данных int
+        day, month, year = int(day), int(month), int(year)
+        return day, month, year
+
+    day, month, year = preprocessing_date(date)
+
+    # Функция для проверки корректности даты
+    def check_date(day, month, year):
+        # Проверяем день, месяц и год на целочисленность
+        if (
+            (type(day) is not int)
+            or (type(month) is not int)
+            or (type(year) is not int)
+        ):
+            return False
+        # Проверяем год на заданный диапазон
+        if (year <= 1900) or (year >= 2022):
+            return False
+        # Проверяем месяц на заданный диапазон
+        if (month < 1) or (month > 12):
+            return False
+        # Проверяем день на заданный диапазон
+        if (day < 1) or (day > 31):
+            return False
+        # Проверяем апрель, июнь, сентябрь и ноябрь на количество дней
+        if (month in [4, 6, 9, 11]) and (day > 30):
+            return False
+        # Проверяем количество дней в феврале
+        if month == 2 and day > 28:
+            return False
+        return True
+
+    if not check_date(day, month, year):
+        raise ValueError("Invalid Date!")
+
+    # Если список не был передан — создаём пустой список
+    if registry is None:
+        registry = list()
+    # Разделяем дату на составляющие
+    day, month, year = preprocessing_date(date)
+    # Добавляем данные в список
+    registry.append((surname, name, middle_name, day, month, year))
+    return registry
+
+
+reg = register("Petrova", "Maria", "13.03.2003", "Ivanovna")
+reg = register("Ivanov", "Sergej", "24.09.1995", registry=reg)
+reg = register("Smith", "John", "13.02.2003", registry=reg)
+print(reg)
+## [('Petrova', 'Maria', 'Ivanovna', 13, 3, 2003), ('Ivanov', 'Sergej', None, 24, 9, 1995), ('Smith', 'John', None, 13, 2, 2003)]
+
+# reg = register("Ivanov", "Sergej", "24.13.1995")
+## ValueError: Invalid Date!
